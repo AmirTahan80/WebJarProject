73201e9a95bebe0bfebf5f008e08aa8d852484a4
amirtahan <amirhosin6402@gmail.com>
2023-06-15 03:19:40 +0330

    Add migraions for edit dicount in sql

---
+++ WebJar.Persistence/Migrations/20230614233151_ChangeDiscount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebJar.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDiscount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpireDate",
+                table: "Discounts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Amount",
+                table: "Discounts",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpireDate",
+                table: "Discounts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Amount",
+                table: "Discounts",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+        }
+    }
+}
