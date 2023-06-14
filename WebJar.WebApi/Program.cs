87d274d15e79c8389a6c65e646f2c7c7fb2e28b8
amirtahan <amirhosin6402@gmail.com>
2023-06-15 01:00:45 +0330

    Inject dbcontext in the program

--- WebJar.WebApi/Program.cs
+++ WebJar.WebApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 namespace WebJar.WebApi
 {
     public class Program
@@ -6,10 +9,17 @@ namespace WebJar.WebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            IConfigurationRoot configuration = new ConfigurationBuilder().Build();
             // Add services to the container.
 
             builder.Services.AddControllers();
+            #region DbContext
+            builder.Services.AddDbContext<WebJar.Persistence.Data.AppContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ConnectToDataBase"),
+                    o => o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
+            });
+            #endregion
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
