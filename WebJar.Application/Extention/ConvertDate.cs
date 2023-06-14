b3133c38db5f792917bbd27397c4d3310fa44379
amirtahan <amirhosin6402@gmail.com>
2023-06-15 03:18:40 +0330

    Add Extentions

---
+++ WebJar.Application/Extention/ConvertDate.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace WebJar.Application.Extention
+{
+    public static class ConvertDate
+    {
+        /// <summary>
+        /// Return Year - Month (1400 - 3)
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string GetMonthAndYear(DateTime time)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            var getMonthAndYear = persianCalendar.GetYear(time) + "-" + persianCalendar.GetMonth(time);
+            return getMonthAndYear;
+        }
+        /// <summary>
+        /// Return Year , Month , Day , Hour , Minute , Second , MiliSecond
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static DateTime GetDate(DateTime time)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            var curentDate = persianCalendar.ToDateTime(time.Year, time.Month, time.Day, time.Hour, time.Minute, time.Second, time.Millisecond);
+            return curentDate;
+        }
+        /// <summary>
+        /// Return Year , Month , Day 
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static DateTime GetDateYeadAndMonthAndDay(DateTime time)
+        {
+            PersianCalendar persianCalendar = new PersianCalendar();
+            var curentDate = persianCalendar.GetYear(time) + "/" + persianCalendar.GetMonth(time) + "/" + persianCalendar.GetDayOfMonth(time);
+            var convert = ParsePersianDate(curentDate);
+            return convert;
+        }
+
+        private static DateTime ParsePersianDate(this string date)
+        {
+            var dateParts = date.Split('/');
+            return new DateTime(int.Parse(dateParts[0]), int.Parse(dateParts[1]), int.Parse(dateParts[2]), new PersianCalendar());
+        }
+    }
+}
---
+++ WebJar.Application/Extention/FileUpload.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace WebJar.Application.Extention
+{
+    public static class FileUpload
+    {
+        /// <summary>
+        /// This methode upload photo and return path of the image.
+        /// </summary>
+        public static string UploadImage(IFormFile file, string placeFile)
+        {
+            var date = ConvertDate.GetMonthAndYear(DateTime.Now);
+            string folder = $@"wwwroot\Images\{placeFile}\{date}\";
+            var uploadsRootFolder = Path.Combine(Directory.GetCurrentDirectory(), folder);
+            if (!Directory.Exists(uploadsRootFolder))
+            {
+                Directory.CreateDirectory(uploadsRootFolder);
+            }
+
+            string fileName = DateTime.Now.Ticks.ToString() + "-" + file.FileName;
+            string filePath = Path.Combine(uploadsRootFolder, fileName);
+            using (var FileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(FileStream);
+            }
+            return (placeFile + "/" + date + "/" + fileName);
+        }
+        public static string UploadImages(IEnumerable<IFormFile> files, string placeFile)
+        {
+            var date = ConvertDate.GetMonthAndYear(DateTime.Now);
+            string folder = $@"wwwroot\Images\{placeFile}\{date}\";
+            var uploadsRootFolder = Path.Combine(Directory.GetCurrentDirectory(), folder);
+            if (!Directory.Exists(uploadsRootFolder))
+            {
+                Directory.CreateDirectory(uploadsRootFolder);
+            }
+            string imagesPath = "";
+            foreach (var file in files)
+            {
+                string fileName = DateTime.Now.Ticks.ToString() + "-" + file.FileName;
+                string filePath = Path.Combine(uploadsRootFolder, fileName);
+                using (var FileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(FileStream);
+                }
+                imagesPath = placeFile + "/" + date + "/" + fileName + ",";
+            }
+            return imagesPath;
+        }
+    }
+}
