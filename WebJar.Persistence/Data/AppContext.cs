0c68e39f867c27abf5a83eaef420c0a750c8d04e
amirtahan <amirhosin6402@gmail.com>
2023-06-15 00:41:16 +0330

    Install EFcore and create dbContext

---
+++ WebJar.Persistence/Data/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebJar.Domain.Entities;
+
+namespace WebJar.Persistence.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<PropertyValue> PropertyValues { get; set; }
+        public DbSet<AddOn> AddOns { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+    }
+}
